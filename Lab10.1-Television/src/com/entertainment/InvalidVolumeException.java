98a75b72d00ed706dc20191a1f9267bd9bc2a594
RaylanRod <Raylan.Rodriguez@tlglearning.com>
2023-05-10 12:53:20 -0700

    Lab10.1-Television practice on the throwing side

---
+++ Lab10.1-Television/src/com/entertainment/InvalidVolumeException.java
@@ -0,0 +1,21 @@
+package com.entertainment;
+/*
+ *Application-specific custom exception class - CHECKED EXCEPTION.
+ */
+public class InvalidVolumeException extends Exception{
+
+    public InvalidVolumeException() {
+    }
+
+    public InvalidVolumeException(String message) {
+        super(message);
+    }
+
+    public InvalidVolumeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidVolumeException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ Lab10.1-Television/src/com/entertainment/client/TelevisionClient.java
@@ -0,0 +1,16 @@
+package com.entertainment.client;
+
+import com.entertainment.InvalidVolumeException;
+import com.entertainment.Television;
+
+class TelevisionClient {
+
+    public static void main(String[] args) {
+        try {
+            Television tv1  = new Television("Sony", -50);
+            System.out.println(tv1);
+        } catch (InvalidVolumeException e) {
+            e.printStackTrace();
+        }
+    }
+}
